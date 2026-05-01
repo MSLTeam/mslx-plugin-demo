55d8f33389c2b64feb90b6ed661d2cffc99da214
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-05-01 17:28:26 +0800

    feat: 更新SDK版本 & 新增ICON参数

--- MSLXPluginEntry.cs
+++ MSLXPluginEntry.cs
@@ -6,11 +6,12 @@ public class MSLXPluginEntry : IPlugin
 {
     public string Id => "mslx-plugin-demo"; 
     public string Name => "MSLX 示例插件";
-    public string Description => "MSLX 示例插件";
-    public string Version => "1.0.2";
-    public string MinLoaderVersion => "1.3.8.2";
-    public string Developer => "xiaoyu";
-    public string AuthorUrl => "https://github.com/MSLTeam";
+    public string Description => "这是MSLX的示例插件，简单演示了一些插件可实现的功能，可以前往Github查看具体实现~";
+    public string Version => "1.0.3";
+    public string Icon => "icon.png";
+    public string MinSDKVersion => "1.4.0";
+    public string Developer => "luluxiaoyu";
+    public string AuthorUrl => "https://mslx.mslmc.cn/plugin-dev/init/start/";
     public string PluginUrl => "https://github.com/MSLTeam/mslx-plugin-demo";
 
     public void OnLoad()
